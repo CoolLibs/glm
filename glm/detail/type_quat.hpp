ada5a4465850f56874a70a6ec4df8949e5049b92
Jules Fouchy <jules.fouchy@ntymail.com>
2021-11-14 21:01:42 +0100

    Silence a warning on msvc

--- glm/detail/type_quat.hpp
+++ glm/detail/type_quat.hpp
@@ -3,6 +3,8 @@
 
 #pragma once
 
+#pragma warning(disable:4201) // nonstandard extension used: nameless struct/union
+
 // Dependency:
 #include "../detail/type_mat3x3.hpp"
 #include "../detail/type_mat4x4.hpp"
